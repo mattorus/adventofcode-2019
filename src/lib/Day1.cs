d0044781ab85dc6ab6b7e946b5268618ece09968
tomithychen <tomithy@gmail.com>
2019-12-09 20:25:25 -0800

    day 1 done (#2)

---
+++ src/lib/Day1.cs
@@ -0,0 +1,61 @@
+﻿namespace AdventOfCode2019
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using System.Text;
+
+    public static class Day1
+    {
+        /// <summary>
+        /// Gets the total fuel requirements of all of the given modules.
+        /// </summary>
+        /// <param name="moduleMasses">Input file containing all module masses.</param>
+        /// <param name="includeFuelMass">Whether to include the fuel as part of the mass.</param>
+        /// <returns>Total fuel requirements.</returns>
+        public static int GetFuelRequirements(FileInfo moduleMasses, bool includeFuelMass = false)
+        {
+            _ = moduleMasses ?? throw new ArgumentNullException(nameof(moduleMasses));
+            if (!moduleMasses.Exists)
+            {
+                throw new FileNotFoundException($"{nameof(moduleMasses)} was not found!", moduleMasses.FullName);
+            }
+
+            var totalFuelRequired = 0;
+            foreach (var mass in File.ReadAllLines(moduleMasses.FullName).Select(x => int.Parse(x)))
+            {
+                totalFuelRequired += GetModuleFuelRequirement(mass, includeFuelMass);
+            }
+
+            return totalFuelRequired;
+        }
+
+        /// <summary>
+        /// Gets fuel required to launch a given module based on its mass.
+        /// </summary>
+        /// <param name="mass">Mass of module.</param>
+        /// <param name="includeFuelMass">Whether to include the fuel as part of the mass.</param>
+        /// <returns>Fuel required to launch module.</returns>
+        /// <remarks>To find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.</remarks>
+        internal static int GetModuleFuelRequirement(int mass, bool includeFuelMass)
+        {
+            var fuelRequired = mass / 3 - 2;
+            if (includeFuelMass)
+            {
+                var additionalFuel = fuelRequired;
+                while (additionalFuel > 0)
+                {
+                    additionalFuel = GetModuleFuelRequirement(additionalFuel, includeFuelMass: false);
+                    if (additionalFuel > 0)
+                    {
+                        fuelRequired += additionalFuel;
+                    }
+                }
+            }
+
+            return fuelRequired;
+        }
+    }
+}
---
+++ src/test/Day1Tests.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2019.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    using AdventOfCode2019;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Test class for Day1
+    /// </summary>
+    [TestClass]
+    public class Day1Tests
+    {
+        private const string InputFile = @".\input\Day1.txt";
+
+        /// <summary>
+        /// Day1 Part1
+        /// </summary>
+        [TestMethod]
+        public void Part1()
+        {
+            Console.WriteLine($"Fuel Required: {Day1.GetFuelRequirements(new FileInfo(InputFile))}");
+        }
+
+        /// <summary>
+        /// Day1 Part2
+        /// </summary>
+        [TestMethod]
+        public void Part2()
+        {
+            Console.WriteLine($"Fuel Required: {Day1.GetFuelRequirements(new FileInfo(InputFile), includeFuelMass: true)}");
+        }
+    }
+}
